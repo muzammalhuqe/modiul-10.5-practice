73240a9475c71c8c406ae14875736521af5fc467
md muzammal huqe <muzammalraj@gmail.com>
2023-12-12 13:58:41 +0600

    filtering updated

--- practice_page_navigation/practice_page_navigation/views.py
+++ practice_page_navigation/practice_page_navigation/views.py
@@ -1,5 +1,7 @@
 
 from django.shortcuts import render
+import datetime
 
 def home(request):
-    return render(request, 'home.html')
+    dummydata = {'name' : 'muzammel', 'age' : 25, 'city' : 'rajshahi', 'lerning' : 'i am lerning django framework', 'date' : datetime.datetime.now()}
+    return render(request, 'home.html', dummydata)
